70dc72b0870c016a96cebfb55a390ea11277a421
Florian Bersier <info@xpressyoo.com>
2013-04-08 15:23:26 +0200

    modified:   _includes/themes/fb/default.html 	modified:   _site/archive.html 	modified:   _site/assets/themes/fb/css/blog.css 	modified:   _site/assets/themes/fb/js/blog.js 	modified:   _site/atom.xml 	modified:   _site/categories.html 	modified:   _site/elysee-2012-hadopi/index.html 	modified:   _site/etrust-homo-facebookiens/index.html 	modified:   _site/facebook-bug/index.html 	modified:   _site/i-know-who-you-are/index.html 	modified:   _site/implementation-social-sharing-jekyll/index.html 	modified:   _site/index.html 	modified:   _site/pages.html 	modified:   _site/primaires-socialistes/index.html 	modified:   _site/sitemap.txt 	modified:   _site/sitemap.xml 	modified:   _site/tags.html 	modified:   assets/themes/fb/css/blog.css 	modified:   assets/themes/fb/js/blog.js

--- _site/assets/themes/fb/js/blog.js
+++ _site/assets/themes/fb/js/blog.js
@@ -7,6 +7,18 @@ $(function() {
             $('#life li:eq(' + i + ')').removeClass('p0').addClass('seld');
         }
     }
+    // Color Randomizer
+    /*var array = ["a", "b", "c"];
+    function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    	}
+    return array[0];
+	}
+    $('.page-header h1').addClass(shuffleArray(array));*/
     //
     if ($('#blog-index').length) {
         $('#xcf').css('display', 'none');
@@ -15,22 +27,24 @@ $(function() {
         $('#featured').fadeIn('slow').addClass('go');
     });
     $('#closef').on('click', function() {
-        $('#featured').fadeOut('slow').empty();
+        $('#featured').fadeOut('slow');
     });
     //////////Youtube
     var id;
     var video = document.getElementById('vide0');
     if (video === undefined) {
         id = 'Xqw4wo8vdY8';
+        nb = '3';
     } else {
         id = video.getAttribute('data-id');
+        nb = video.getAttribute('data-nb');
     }
     $.ajax({
         type: 'GET',
         url: 'http://gdata.youtube.com/feeds/api/videos/' + id + '?&v=2&alt=jsonc',
         dataType: 'jsonp',
         success: function(yt) {
-            var title0 = yt.data.title.split(' ').splice(0, 3).join(' ').replace(/[`~!@#$%\^&*()_|+\-=?;:,.<>\{\}\[\]\\\/]/gi, '');
+            var title0 = yt.data.title.split(' ').splice(0, nb).join(' ').replace(/[`~!@#$%\^&*()_|+\=?;:,.<>\{\}\[\]\\\/]/gi, '');
             var title = yt.data.title;
             var play = 0;
             $("#track").append(title0);
